333180c48691ac628f103fc18b425e6e159814af
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-11-07 13:28:51 +0800

    finish exercise 7.7

---
+++ country-hooks/src/App.jsx
@@ -0,0 +1,88 @@
+import  { useState, useEffect } from 'react'
+import axios from 'axios'
+
+
+const useField = (type) => {
+  const [value, setValue] = useState('')
+
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  return {
+    type,
+    value,
+    onChange
+  }
+}
+
+const useCountry = (name) => {
+  const [result, setResult] = useState(null)
+  const [data, setData] = useState(null)
+  const [found, setFound] = useState(false)
+
+  useEffect(() => {
+    axios.get(`https://studies.cs.helsinki.fi/restcountries/api/name/${name}`).then((response) => {
+      console.log('response',response.data)
+      setData(response.data);
+      setFound(true)
+  }).catch(error => console.log(error))
+}, [name])
+  
+  if (data) {
+    console.log(data.name)
+  }
+  
+  return {
+    found,
+    data
+  }
+}
+
+const Country = ({ country }) => {
+
+  if (!country) {
+    return null
+  }
+
+  if (!country.found) {
+    return (
+      <div>
+        not found...
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <h3>{country.data.name.common} </h3>
+      <div>capital {country.data.capital} </div>
+      <div>population {country.data.population}</div> 
+      <img src={country.data.flags.png} height='100' alt={`flag of ${country.data.name.common}`}/>  
+    </div>
+  )
+}
+
+const App = () => {
+  const nameInput = useField('text')
+  const [name, setName] = useState('')
+  const country = useCountry(name)
+
+  const fetch = (e) => {
+    e.preventDefault()
+    setName(nameInput.value)
+  }
+
+  return (
+    <div>
+      <form onSubmit={fetch}>
+        <input {...nameInput} />
+        <button>find</button>
+      </form>
+
+      <Country country={country} />
+    </div>
+  )
+}
+
+export default App
---
+++ country-hooks/src/hooks/index.js
@@ -0,0 +1,7 @@
+import { useState } from "react"
+
+export const useCountry = ( ) => {
+    const [country, setCountry] = useState('')
+
+    
+}
