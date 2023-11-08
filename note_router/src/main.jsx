80b8ddbe7914d445547456face3bfe4f0a9a239d
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-11-08 09:58:46 +0800

    Add styled-components

--- note_router/src/main.jsx
+++ note_router/src/main.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 import ReactDOM from 'react-dom/client'
 import { useState } from 'react'
-
 import {
   BrowserRouter as Router,
   Routes,
@@ -12,6 +11,35 @@ import {
   useNavigate,
   useMatch
 } from "react-router-dom"
+import styled from 'styled-components'
+
+const Button = styled.button`
+  background: Bisque;
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid Chocolate;
+  border-radius: 3px;
+`
+
+const Input = styled.input`
+  margin: 0.25em;
+`
+const Page = styled.div`
+  padding: 1em;
+  background: papayawhip;
+`
+
+const Navigation = styled.div`
+  background: BurlyWood;
+  padding: 1em;
+`
+
+const Footer = styled.div`
+  background: Chocolate;
+  padding: 1em;
+  margin-top: 1em;
+`
 
 
 const Home = () => (
@@ -70,12 +98,12 @@ const Login = (props) => {
       <h2>login</h2>
       <form onSubmit={onSubmit}>
         <div>
-          username: <input />
+          username: <Input />
         </div>
         <div>
-          password: <input type='password' />
+          password: <Input type='password' />
         </div>
-        <button type="submit">login</button>
+        <Button type="submit">login</Button>
       </form>
     </div>
   )
@@ -120,8 +148,8 @@ const App = () => {
     : null
 
   return (
-    <div>
-        <div>
+    <Page>
+        <Navigation>
           <Link style={padding} to="/">home</Link>
           <Link style={padding} to="/notes">notes</Link>
           <Link style={padding} to="/users">users</Link>
@@ -129,7 +157,7 @@ const App = () => {
             ? <em>{user} logged in</em>
             : <Link style={padding} to="/login">login</Link>
           }
-        </div>
+        </Navigation>
 
         <Routes>
           <Route path="/notes/:id" element={<Note note={note} />} />
@@ -138,11 +166,11 @@ const App = () => {
           <Route path="/login" element={<Login onLogin={login} />} />
           <Route path="/" element={<Home />} />
         </Routes>
-      <div>
+      <Footer>
         <br />
         <em>Note app, Department of Computer Science 2023</em>
-      </div>
-    </div>
+      </Footer>
+    </Page>
   )
 }
 
