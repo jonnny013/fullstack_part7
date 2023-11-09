d932796337d7cbb8bba579b004e962b7ff6c0f64
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-11-09 10:49:42 +0800

    Add notificationReducer

---
+++ bloglist-frontend-redux/src/reducers/notificationReducer.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState: {
+    message: '',
+    style: '',
+  },
+  reducers: {
+    notification(state, action) {
+      state.message = action.payload
+      state.style = 'style1'
+    },
+    error(state, action) {
+      state.message = action.payload
+      state.style = 'style2'
+    },
+    reset(state) {
+      state.message = ''
+      state.style = ''
+    }
+  },
+})
+
+export const { notification, error, reset } = notificationSlice.actions
+
+export default notificationSlice.reducer
