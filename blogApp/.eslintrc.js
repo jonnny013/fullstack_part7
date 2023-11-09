a680bcffa043b47b5072891df4a04a12402f1b71
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-11-09 09:45:26 +0800

    configure prettier

--- blogApp/.eslintrc.js
+++ blogApp/.eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
         'es2021': true,
         'jest': true,
     },
-    'extends': 'eslint:recommended',
+    'extends': ['eslint:recommended', 'prettier'],
     'overrides': [
     ],
     'parserOptions': {
