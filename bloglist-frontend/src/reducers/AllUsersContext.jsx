19995106de291bc2fed589e092941e50f7dc39b1
Jon Love <124639155+jonnny013@users.noreply.github.com>
2023-11-13 11:27:03 +0800

    Add all Users reducer

---
+++ bloglist-frontend/src/reducers/AllUsersContext.jsx
@@ -0,0 +1,33 @@
+import { useReducer, createContext, useContext } from 'react'
+
+const allUsersReducer = (state, action) => {
+  switch (action.type) {
+  case 'users':
+    return (state = action.payload)
+  default:
+    return state
+  }
+}
+
+const AllUsersContext = createContext()
+
+export const AllUsersContextProvider = props => {
+  const [allUsers, allUsersDispatch] = useReducer(allUsersReducer, null)
+  return (
+    <AllUsersContext.Provider value={[allUsers, allUsersDispatch]}>
+      {props.children}
+    </AllUsersContext.Provider>
+  )
+}
+
+export const useAllUsersValue = () => {
+  const allUsersAndDispatch = useContext(AllUsersContext)
+  return allUsersAndDispatch[0]
+}
+
+export const useAllUsersDispatch = () => {
+  const allUsersAndDispatch = useContext(AllUsersContext)
+  return allUsersAndDispatch[0]
+}
+
+export default AllUsersContext
